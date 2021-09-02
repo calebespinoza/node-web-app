2a57da5f038d36b05a1287858c2d7627116ef01f
Caleb Espinoza G <caleb.espinoza@outlook.com>
2021-09-02 05:30:09 -0400

    Fix deployment script

--- deployment.sh
+++ deployment.sh
@@ -2,11 +2,11 @@
 echo "Retrieving the Container ID if exits"
 container1=$(docker ps --filter status=running --filter name=node-web-app* -q | head -n 1)
 container2=$(docker ps --filter status=running --filter name=node-web-app* -q | tail -1)
-echo "Container 1 = $container1"
-echo "Container 2 = $container2"
 
 if [ ! -z $container1 -a ! -z $container2 ];
 then
+    echo "Container 1 = $container1"
+    echo "Container 2 = $container2"
     echo "Retrieving the Image Name associated to the container"
     imageName=$(docker inspect --format='{{.Config.Image}}' $container1)
     echo "ImageName = $imageName"
